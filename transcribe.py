be0cb8e2d3e59d8e34f0bb74d8d8843dc3455f70
Tommy Lees <thomas.lees112@gmail.com>
2024-11-09 14:54:08 +0000

    set language, return the whole result dict

--- transcribe.py
+++ transcribe.py
@@ -6,16 +6,31 @@ from loguru import logger
 
 
 def transcribe_audio(
-    audio_file: Path, model: Literal["base", "turbo"] = "turbo"
+    audio_file: Path, model_str: Literal["base", "turbo"] = "turbo"
 ) -> list[dict]:
-    logger.info(f"Loading Whisper {model} model...")
-    model = whisper.load_model(model)
+    """Transcribe an audio file using the Whisper model.
+
+    Args:
+        audio_file (Path): The path to the audio file to transcribe.
+        model_str (Literal["base", "turbo"], optional): The model to use for transcription. Defaults to "turbo".
+
+    Returns:
+        list[dict]: The transcription result. Three keys: "segments", "text", and "language".
+    """
+    logger.info(f"Loading Whisper {model_str} model...")
+    model = whisper.load_model(model_str)
 
     logger.info(f"Starting transcription of: {audio_file}")
     try:
-        result = model.transcribe(audio_file, word_timestamps=True)
+        result = model.transcribe(
+            audio_file.as_posix(),
+            word_timestamps=True,
+            verbose=True,
+            language="en",
+        )
         logger.success("Transcription completed successfully")
-        return result["segments"]
+        return result
+
     except Exception as e:
         logger.error(f"Transcription failed: {e}")
         raise
@@ -25,7 +40,7 @@ if __name__ == "__main__":
     from pathlib import Path
 
     audio_file = Path(
-        "/Users/tommylees/Downloads/Taiwan Invasion： Timeline to Global Collapse – @Asianometry & Dylan Patel.mp3"
+        "/Users/tommylees/Downloads/could_the_war_in_ukraine_escalate_into_a_world_war.mp3"
     )
     assert audio_file.exists(), logger.error(f"Audio file not found: {audio_file}")
 
